3db29fee6aa733e5a52b077030aeb79bb96b84a7
Gooberton <129799245+Promethium0@users.noreply.github.com>
2025-03-08 21:11:19 -0500

    update physics

--- obj/Debug/net8.0-windows/SoftBodyMaker.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/SoftBodyMaker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SoftBodyMaker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91c3a12297db2a37dfcd3827ee61acc1c25b8cf0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9168c4a0ca36aa03279c71e331bdd669a3bd29da")]
 [assembly: System.Reflection.AssemblyProductAttribute("SoftBodyMaker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SoftBodyMaker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
